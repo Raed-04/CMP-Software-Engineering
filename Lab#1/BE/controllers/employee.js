9f3c9a5aae1d3331c16bf30ed3ebc63cc695e9bc
Darkosylver <darkosylver27@gmail.com>
2025-02-15 15:32:11 +0200

    It works, but buggy as hell, fixes required

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -8,8 +8,21 @@ exports.getEmployees = async (req, res, next) => {
 
 // TODO
 exports.deleteEmployee = async (req, res, next) => {
+  let index = employee.findIndex(function(element) {{
+    if (element.id === req.params.id)
+    {
+      return true;
+    }
+    return false;
+  }});
+  let spliced = employee.splice(index, 1);
+  console.log("Element removed: ", spliced);
+  res.status(200);
 };
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
+  let Emp = {id: req.body.ID, name: req.body.Name}
+  employee.push(Emp);
+  res.status(200);
 };
--- Lab#1/FE/index.js
+++ Lab#1/FE/index.js
@@ -24,6 +24,9 @@ function fetchEmployees() {
         row.appendChild(deleteCell)
 
         tableBody.appendChild(row)
+
+        //Deletion event listener
+        deleteButton.addEventListener("click", function() { deleteEmployee(item.id) });
       })
     })
     .catch(error => console.error(error))
@@ -31,22 +34,41 @@ function fetchEmployees() {
 
 // TODO
 // add event listener to submit button
+let button = document.getElementById("sub");
+button.addEventListener("click", createEmployee);
 
 // TODO
 // add event listener to delete button
+// Added the listener right after the creation of the button 
 
 // TODO
 function createEmployee (){
   // get data from input field
   // send data to BE
   // call fetchEmployees
+  let nameBox = document.getElementById("name");
+  let IDBox = document.getElementById("id");
+  let employeeName = nameBox.value;
+  let employeeID = IDBox.value;
+  fetch('http://localhost:3000/api/v1/employee', {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    },
+    body: JSON.stringify({ID: employeeID, Name: employeeName})
+  });
+  fetchEmployees();
 }
 
 // TODO
-function deleteEmployee (){
+function deleteEmployee (ID){
   // get id
   // send id to BE
   // call fetchEmployees
+  fetch(`http://localhost:3000/api/v1/employee/${ID}`, {
+    method: "DELETE"
+  });
+  fetchEmployees();
 }
 
-fetchEmployees()
+fetchEmployees();
